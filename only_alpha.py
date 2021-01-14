a860e6d878cd17b8b07337ca62f0e8b5ecc196eb
kuganveluswamy <kuganveluswamy@gmail.com>
2021-01-13 20:51:53 -0800

    added code snippet of method using isalpha()

--- only_alpha.py
+++ only_alpha.py
@@ -1,10 +1,17 @@
 #! /usr/bin/python3
 import re
+
+# Example sentence
+sentence = "Punctuation matters: Eat your dinner! vs Eat. You're dinner!"
+
+# Method 1 using regex match
 # Compile a pattern that matches every character that is not an alphabet (i.e a-z A-Z)
 pattern = re.compile('[^a-zA-Z]')
-# Example string
-sentence = "Punctuation matters: Eat your dinner! vs Eat. You're dinner!"
-print("Sentence before removing non alphabets: {}".format(sentence))
-# Substituting every non alphabet with ''
-result = pattern.sub('', sentence)
-print("Sentence with only alphabets: {}".format(result))
+print("Sentence before removing non alphabets: {}\n".format(sentence))
+# Remove every non alphabet with substitution
+result = pattern.sub(' ', sentence)
+print("Regex Method >\n Sentence with only alphabets: {}".format(result))
+
+# Method 2 using isaplha()
+result = ''.join([letter if letter.isalpha() else ' ' for letter in sentence])
+print("isaplha() Method >\n Sentence with only alphabets: {}".format(result))
